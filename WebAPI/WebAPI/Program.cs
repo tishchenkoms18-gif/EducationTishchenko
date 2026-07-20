2ac3f3455d0af516b6e541e4625f71bd39e2446a
MariiaStich <tishchenko2014@mail.ru>
2026-07-20 07:57:26 +0300

    fea: added middleware Global Exception Handling

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -2,13 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+//builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Потому что данные в памяти 
 builder.Services.AddSingleton<IEventService, EventService>();
 var app = builder.Build();
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
